b068f6ef0c15e9c373da845c35224cda1cf1c09c
caoli5288 <caoli5288@gmail.com>
2018-07-13 16:37:40 +0800

    Always use built-in zlib

--- proxy/src/main/java/net/md_5/bungee/compress/PacketCompressor.java
+++ proxy/src/main/java/net/md_5/bungee/compress/PacketCompressor.java
@@ -3,22 +3,31 @@ package net.md_5.bungee.compress;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-import java.util.zip.Deflater;
 import lombok.Setter;
 import net.md_5.bungee.jni.zlib.BungeeZlib;
+import net.md_5.bungee.jni.zlib.JavaZlib;
 import net.md_5.bungee.protocol.DefinedPacket;
 
 public class PacketCompressor extends MessageToByteEncoder<ByteBuf>
 {
 
-    private final BungeeZlib zlib = CompressFactory.zlib.newInstance();
+    private final BungeeZlib zlib = new JavaZlib();
     @Setter
     private int threshold = 256;
+    private int level;
+
+    public PacketCompressor(int level) {
+        this.level = level;
+    }
+
+    public PacketCompressor() {
+        this(3);
+    }
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception
     {
-        zlib.init( true, Deflater.DEFAULT_COMPRESSION );
+        zlib.init( true, level);
     }
 
     @Override
